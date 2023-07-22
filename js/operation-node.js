1d741fafc42299acf3e0fb89dd14e3126289bc15
Michael Poutre <m1kep.my.mail@gmail.com>
2023-07-22 15:07:05 -0700

    fix: Use node.onAdded callback instead of a mix of configure and created

--- js/operation-node.js
+++ js/operation-node.js
@@ -4,44 +4,50 @@ app.registerExtension({
     name: "ComfyLiterals.OperationNode",
     nodeCreated(node, app) {
         if (node['comfyClass'] === 'Operation') {
-            /**
-             * @type {Record<string, INodeInputSlot>}
-             */
-            const inputCache = {
-                "A": node.inputs[1],
-                "B": node.inputs[3]
-            }
+            const onAdded = node.onAdded
+            node.onAdded = function (graph) {
+                console.log("OperationNode onAdded")
+                const firstCallbackResp = onAdded ? onAdded.apply(this, arguments) : undefined;
 
-            // Node has 4 inputs(IntA, FloatA, IntB, FloatB)
-            // Remove both float inputs, Float B moves to index 2 after Float A is removed
-            node.removeInput(1)
-            node.removeInput(2)
-
-            // Add a toggle widget to the node
-            node.widgets[0].callback = function (v, canvas, node) {
-                addInputAtIndex(node, inputCache["A"], 0)
-                inputCache["A"] = node.inputs[1]
-                node.removeInput(1)
-            }
-            node.widgets[1].callback = function (v, canvas, node) {
-                addInputAtIndex(node, inputCache["B"], 2)
-                inputCache["B"] = node.inputs[1]
-                node.removeInput(1)
-            }
-
-            node.onConfigure = function (nodeInfo) {
-                // Correct the cache if the inputs have been changed
-                // This is needed for copy/paste to work correctly
-                if (nodeInfo.inputs[0].name === "A - Int") {
-                    inputCache["A"] = {name: "A - Float", type: "FLOAT", link: null}
-                } else if (nodeInfo.inputs[0].name === "A - Float") {
-                    inputCache["A"] = {name: "A - Int", type: "INT", link: null}
+                /**
+                 * @type {Record<string, INodeInputSlot>}
+                 */
+                const inputCache = {
+                    "A": node.inputs[1],
+                    "B": node.inputs[3]
                 }
 
-                if (nodeInfo.inputs[1].name === "B - Int") {
-                    inputCache["B"] = {name: "B - Float", type: "FLOAT", link: null}
-                } else if (nodeInfo.inputs[1].name === "B - Float") {
-                    inputCache["B"] = {name: "B - Int", type: "INT", link: null}
+                if (this.widgets_values) {
+                    const aType = this.widgets_values[0]
+                    const bType = this.widgets_values[1]
+
+                    // [IntA, FloatA, IntB, FloatB]
+                    const aIdxToDelete = aType === "INT" ? 1 : 0
+                    // [*A, IntB, FloatB]
+                    const bIdxToDelete = bType === "INT" ? 3 : 1
+
+                    inputCache["A"] = node.inputs[aIdxToDelete]
+                    this.removeInput(aIdxToDelete)
+                    inputCache["B"] = node.inputs[bIdxToDelete]
+                    this.removeInput(bIdxToDelete)
+                } else {
+                    // Nodes being restored/pasted don't have widget_values
+                    // Node has 4 inputs(IntA, FloatA, IntB, FloatB)
+                    // Remove both float inputs, Float B moves to index 2 after Float A is removed
+                    this.removeInput(1)
+                    this.removeInput(2)
+                }
+
+                // Add a toggle widget to the node
+                this.widgets[0].callback = function (v, canvas, node) {
+                    addInputAtIndex(node, inputCache["A"], 0)
+                    inputCache["A"] = node.inputs[1]
+                    node.removeInput(1)
+                }
+                this.widgets[1].callback = function (v, canvas, node) {
+                    addInputAtIndex(node, inputCache["B"], 2)
+                    inputCache["B"] = node.inputs[1]
+                    node.removeInput(1)
                 }
             }
         }
