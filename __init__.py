bdddb08ca82d90d75d97b1d437a652e0284a32ac
Michael Poutre <m1kep.my.mail@gmail.com>
2023-11-19 17:08:18 -0800

    fix: Add custom name for "String" literal to avoid conflicts

--- __init__.py
+++ __init__.py
@@ -6,9 +6,14 @@ NODE_CLASS_MAPPINGS = {
     "Int": IntLiteral,
     "Float": FloatLiteral,
     "String": StringLiteral,
+    "KepStringLiteral": StringLiteral,
     "Operation": Operation,
     "Checkpoint": CheckpointListLiteral,
     "Lora": LoraListLiteral,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "KepStringLiteral": "String",
 }
 
 EXTENSION_NAME = "ComfyLiterals"
